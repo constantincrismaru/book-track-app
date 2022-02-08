9e3552134060cee39ac690be806a46792c912158
constantincrismaru <constantincrismarupfa@gmail.com>
2022-02-08 18:01:17 +0200

    updated books route

--- src/components/BookDetails/index.js
+++ src/components/BookDetails/index.js
@@ -6,22 +6,24 @@ const BookDetails = ({ book }) => {
 
     }, [])
     return <div className='book-details'>
-        <div>
-            <span>Title: {book.title}</span>
-        </div>
-        <div>
-            <span>Categories: {book.categories.map((c, i) => <div key={i} className='sub-item'>
-                {c}
-            </div>)}</span>
-        </div>
-        <div>
-            <span>Authors: {book.authors.map((a, i) => <div key={i} className='sub-item'>
-                {a}
-            </div>)}</span>
-        </div>
-        <div>
-            <img src={book.thumbnailURL} alt='Book cover not found!'></img>
-        </div>
+        {(book && <>
+            <div>
+                <span>Title: {book.title}</span>
+            </div>
+            <div>
+                <span>Categories: {book.categories.map((c, i) => <div key={i} className='sub-item'>
+                    {c}
+                </div>)}</span>
+            </div>
+            <div>
+                <span>Authors: {book.authors.map((a, i) => <div key={i} className='sub-item'>
+                    {a}
+                </div>)}</span>
+            </div>
+            <div>
+                <img src={book.thumbnailURL} alt='Book cover not found!'></img>
+            </div>
+        </>) || 'Book not found.'}
     </div>
 }
 export default BookDetails
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />} />
+        <Route path='/' element={<Books />} />
         <Route path='books'>
           <Route index element={<Books />} />
           <Route path=':id' element={<BookDetails />} />
